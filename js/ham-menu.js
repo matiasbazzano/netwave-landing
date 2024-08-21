577f835bea6bb26e1f0043304f34c2ad1dd6c9ea
Matias Bazzano <matiasbazzano19@gmail.com>
2024-08-21 18:40:01 -0300

    intial commit

---
+++ js/ham-menu.js
@@ -0,0 +1,30 @@
+// Selecciona los elementos
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu a');
+
+// Función para cerrar el menú
+function closeMenu() {
+    menu.classList.remove('active');
+    hamburgerMenu.classList.remove('open');
+}
+
+// Abre y cierra el menú al hacer clic en el botón
+hamburgerMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    hamburgerMenu.classList.toggle('open');
+});
+
+// Cierra el menú al hacer clic en un enlace del menú
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        closeMenu();
+    });
+});
+
+// Cierra el menú al hacer clic fuera de él
+document.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+        closeMenu();
+    }
+});
---
+++ js/type.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: [
+            "Dynamic websites that captivate",
+            "Engaging digital experiences",
+            "Innovative solutions for growth",
+            "Responsive designs for any device",
+            "Interactive platforms to connect",
+            "Creative solutions for your brand",
+            "Functional sites enhancing UX",
+            "Visually stunning web presence",
+            "Cutting-edge web solutions",
+            "Seamless digital experiences",
+            "High-performance websites",
+            "Elegant designs that resonate",
+            "Powerful online tools for growth"
+        ],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 1000,
+        startDelay: 500,
+        loop: true,
+        cursorChar: '|',
+    };
+    var typed = new Typed('#typed-output', options);
+});
